a77cafe7904ed82c069ea1d4d8aa134b1d3cd1a9
zamit00 <zamit00@gmail.com>
2025-06-03 05:31:26 +0300

    Add files via upload

---
+++ FetchOzarApi.js
@@ -0,0 +1,47 @@
+//gemel datagov
+    fetch('https://data.gov.il/api/3/action/datastore_search?resource_id=a30dcbea-a1d2-482c-ae29-8f781f5025fb&limit=32000')
+  .then(response => response.json())
+  .then(data => {
+   const array = Object.values(data.result);
+   const nestedArray = array[5];
+   console.log(nestedArray)
+  console.log(nestedArray[10].FUND_NAME);
+  
+
+   // console.log('Total results:', data.result.total);
+   // console.log('Records:', datanetunimGemelNet.result.records);
+   // console.log(Array.isArray(data));
+  })
+  .catch(error => console.error('Fetch error:', error));
+//pensia datagov
+    fetch('https://data.gov.il/api/3/action/datastore_search?resource_id=6d47d6b5-cb08-488b-b333-f1e717b1e1bd&limit=32000')
+  .then(response => response.json())
+  .then(data => {
+    datanetunimPensiaNet=data;
+  //  console.log('Total results:', data.result.total);
+  //  console.log('Records:', data.result.records);
+  })
+  .catch(error => console.error('Fetch error:', error));
+  
+  //bituach datagov
+    fetch('https://data.gov.il/api/3/action/datastore_search?resource_id=c6c62cc7-fe02-4b18-8f3e-813abfbb4647&limit=32000')
+  .then(response => response.json())
+  .then(data => {
+    datanetunimBituachNet=data;
+   // console.log('Total results:', data.result.total);
+   // console.log('Records:', data.result.records);
+  })
+  .catch(error => console.error('Fetch error:', error));
+// פעולת סיכום
+  const totals = datanetunimKlaliXM
+  .filter(obj => obj.mozar === 'קרנות השתלמות')
+  .reduce(
+    (acc, item) => {
+      acc.hafkadot += parseFloat(item.hafkadot);
+      acc.meshichot += parseFloat(item.meshichot);
+      acc.niyudNeto += parseFloat(item.niyudNeto);
+      acc.zviraNeto += parseFloat(item.zviraNeto);
+      return acc;
+    },
+    { hafkadot: 0, meshichot: 0, niyudNeto: 0, zviraNeto: 0 }
+  );
